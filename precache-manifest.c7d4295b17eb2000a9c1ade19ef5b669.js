b6423e9aff21a4ad49cb72a34f8626eba709fde5
Powermaxisme <powermaxisme@gmail.com>
2020-02-03 17:17:56 +0700

    Updates

---
+++ precache-manifest.c7d4295b17eb2000a9c1ade19ef5b669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41db6cd1db98ec3dc33062afa3aba1c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "81a87263ca2eb66e7c59",
+    "url": "/monsters-rolodex/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "81a87263ca2eb66e7c59",
+    "url": "/monsters-rolodex/static/js/main.b02cd7d3.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
